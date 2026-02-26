10b8a599b59965e9d2f1ddcf9cb467e6fe0a1cd1
unknown <choprasayansh@gmail.com>
2026-02-26 15:43:09 +0530

    logged in  successfully

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,65 +1,87 @@
 import { useFormik } from "formik";
-import { useState } from "react";
+
 import * as Yup from "yup";
 
 
 export default function Login() {
-  const formik=useFormik({
-    initialValues:{
-    email:"",
-    password:""
-    },
-    validationSchema:schemaValidation,
-   
-  },
-onSubmit=async(values,{resetform})
-);
-
   const schemaValidation=Yup.object({
-    email:Yup.string().required("Email id is required").email("Please enter a valid email"),
-    password:Yup.string().min(8,"minimum 8 characters are required")
+    emailId:Yup.string().required("Email id is required").email("Please enter a valid email"),
+    password:Yup.string()
+    .min(8,"minimum 8 characters are required")
       .matches(/[A-Z]/, "Must contain one uppercase letter")
       .matches(/[a-z]/,"Must contain one lowercase letter")
       .matches(/[0-9]/,"must contain one digit")
       .matches(/[#$%^&*@]/,"must contain a special character")
       .required("password is required")
   })
- 
+  const formik=useFormik({
+    initialValues:{
+    emailId:"",
+    password:""
+    },
+    validationSchema:schemaValidation,
+    onSubmit:async(values,{resetForm})=>{
+      try{
+         const response=await fetch("http://localhost:3000/login",
+            {
+              method:"POST",
+              headers:{
+                "Content-Type":"application/json"
+              },
+              credentials:"include",
+            body:JSON.stringify(values)}
+          );
+         const data=await response.json();
+         console.log(data);
 
-  const handleInputChange = (e) => {
+         if(response.ok){
+          resetForm();
+         }
+
+          
+      }catch(err){
+        console.error("Error :  "+err.message);
+      }
+         
+}
    
   }
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    console.log(formData)
-  }
+);
+
+  
+  
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={formik.handleSubmit}>
       <div className="card w-96 bg-base-200 shadow-xl">
         <div className="card-body">
           <h2 className="card-title text-2xl">Login</h2>
 
           <input
             type="email"
-            name="email"
-            value={formData.email}
+            name="emailId"
+            value={formik.values.emailId}
             onChange={formik.handleChange}
             placeholder="Email"
             className="input input-bordered w-full"
             onBlur={formik.handleBlur}
           />
-
+          {formik.touched.emailId && formik.errors.emailId &&(
+            <p className="text-sm text-red-500">{formik.errors.emailId}</p>
+          )}
           <input
             type="password"
             name="password"
-            value={formData.password}
+            value={formik.values.password}
             onChange={formik.handleChange}
             placeholder="Password"
             className="input input-bordered w-full mt-3"
             onBlur={formik.handleBlur}
           />
+          {formik.touched.password && formik.errors.password && (
+  <p className="text-sm text-red-500">{formik.errors.password}</p>
+)}
 
           <button type="submit" className="btn btn-primary mt-4 w-full">
             Login
